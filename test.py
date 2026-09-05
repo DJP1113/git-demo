36f9cd27b6d70c61b4bb854e3b5d2089e0551802
jiangpingdong <19995773150@163.com>
2026-09-05 15:06:59 +0800

    dev分支，新增求和函数

--- test.py
+++ test.py
@@ -4,3 +4,13 @@ for i in nums:
     if i%2==0:
         eval_list.append(i)
 print(f"筛选出来的偶数列表是{eval_list}")
+#新增新功能
+def cal_sun(a,b):
+    toatal=a+b
+    return toatal
+
+if __name__=="__main__":
+    num=10
+    num2=18
+    res=cal_sun(num1,num2)
+    print("计算结果是{res}")
